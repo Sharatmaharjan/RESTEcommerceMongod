d5af0e874ca5b3f6854160388ba9661e1411c0b0
Sharat Maharjan <sharatmaharjan@gmail.com>
2017-03-29 14:58:47 +0545

    new project started

---
+++ RESTEcommerceMongod/src/main/java/com/restecommercemongod/RestEcommerceMongodApplication.java
@@ -0,0 +1,12 @@
+package com.restecommercemongod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEcommerceMongodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEcommerceMongodApplication.class, args);
+	}
+}
